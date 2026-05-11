4ce1f9fab07b7c935cdeb4d3f33b65dd54b4fd6d
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-11 16:45:40 +0530

    user.js

---
+++ models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "accountant", "classteacher", "student"],
+      default: "student",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
