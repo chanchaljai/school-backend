77eea522d02610ad85a8bb049a4798e69a2a8535
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-11 17:00:30 +0530

    bd and server

---
+++ routes/userRoute.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+// Create user
+router.post("/", createUser);
+
+// Get all users
+router.get("/", getUsers);
+
+// Get single user
+router.get("/:id", getUserById);
+
+// Update user
+router.put("/:id", updateUser);
+
+// Delete user
+router.delete("/:id", deleteUser);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+// DB connect
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("School Management API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
